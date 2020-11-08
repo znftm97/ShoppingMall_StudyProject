2a41330831ef9951bee75a3131fc940cc9a71470
jihoon <znftm97@naver.com>
2020-11-08 16:30:37 +0900

    Member Service Development

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,48 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor // 자동으로 생성자 생성해서 DI 해줌
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /*@Autowired // 생성자가 하나일때는 생략 가능
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }*/
+
+    //회원 가입
+    @Transactional // readOnly = false 가 디폴트값
+    public Long join(Member member){
+        validateDuplicateMember(member); //중복회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    //중복회원 검증
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    //회원 전체 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    //단일 회원 조회
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+}
