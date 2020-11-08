f7e8b850cd564ce6311db003844253eeaf7e72dc
jihoon <znftm97@naver.com>
2020-11-08 18:10:52 +0900

    Member function test

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -31,5 +31,4 @@ public class MemberRepository {
                 .setParameter("name", name)
                 .getResultList();
     }
-
 }
---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,56 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("jh2");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        assertThat(member).isEqualTo(memberRepository.findOne(saveId));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void 중복회원예외() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("jh");
+        Member member2 = new Member();
+        member2.setName("jh");
+
+        //when
+        memberService.join(member1);
+        memberService.join(member2); // 이름이 같으므로 예외 터져야함
+
+        //then
+        Assertions.fail("위 코드에서 예외 발생해야 하는데?? 여기로 오면안댐");
+
+    }
+
+}
