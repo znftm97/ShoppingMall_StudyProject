5ddfa60811a9893a5882a114e80c111fdd39334c
jihoon <znftm97@naver.com>
2020-12-05 01:53:19 +0900

    패치조인으로 성능 최적화 && JPA에서 DTO 바로 조회

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -58,4 +58,14 @@ public class OrderRepository {
         return query.getResultList();
     }
 
+    public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member" +
+                        " join fetch o.delivery", Order.class
+        ).getResultList();
+
+    }
+
+
 }
