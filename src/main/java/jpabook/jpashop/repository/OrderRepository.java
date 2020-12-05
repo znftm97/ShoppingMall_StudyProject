96476894ffe098bc98f2acdf14a51c5cc180b157
jihoon <znftm97@naver.com>
2020-12-05 19:32:14 +0900

    @BatchSize로 페이징 가능

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -76,4 +76,14 @@ public class OrderRepository {
                 " join fetch oi.item i", Order.class
                 ).getResultList();
     }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member" +
+                        " join fetch o.delivery", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
 }
